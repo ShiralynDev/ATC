7541359a2d14667dba4cc1a0c1afaa4450ca091b
SparklesReal <jultomtenriktig@gmail.com>
2024-11-10 17:24:26 +0100

    Cleanup

--- Examples/JS/test.js
+++ Examples/JS/test.js
@@ -1,5 +1,3 @@
-// Note that I, Sparkles am not a JS dev so please help me expand and improve this code
-
 const Wrapper = require('../../ATC/JSWrapper/JSWarpper.js');
 
 if (Wrapper.ATC.thisReturnsOne()) {
--- Examples/Python/main.py
+++ Examples/Python/main.py
@@ -1,4 +1,3 @@
-# Note that I, Sparkles am not a Python dev, please help me expand and improve this code.
 import sys
 sys.path.append('../../ATC/PythonWrapper')
 import PythonWrapper
